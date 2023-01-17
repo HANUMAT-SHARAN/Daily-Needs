335ce51e8da5e3509d5499e05a88fea87db831e4
codersudhir <nandanesudhir1@gmail.com>
2023-01-17 13:48:22 +0530

    fff

---
+++ cw-rct201/src/Components/Homepage/Homepage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProductCategory from './category/CoroselCategory'
+import "./Homepage.css"
+const Homepage = () => {
+  return (
+    <div>
+        <div>
+            <img className='front-flex-img' src="https://images.dailyobjects.com/marche/assets/images/other/republicsale-home-page-desktop.png?tr=cm-pad_resize,v-2,w-1351,dpr-1" alt="" />
+        </div>
+        <div>
+            <img className='front-flex-img' src="https://images.dailyobjects.com/marche/assets/images/other/backpack-desktops.jpg?tr=cm-pad_crop,v-2,w-1351,dpr-1" alt="" />
+        </div>
+   
+    </div>
+  )
+}
+
+export default Homepage
