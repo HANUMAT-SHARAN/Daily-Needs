40873851b1e306505591cd7db761f3aeadffadc0
codersudhir <nandanesudhir1@gmail.com>
2023-01-18 18:14:33 +0530

    fgd

--- cw-rct201/src/Components/Homepage/studiocollection/Collection.tsx
+++ cw-rct201/src/Components/Homepage/studiocollection/Collection.tsx
@@ -10,21 +10,21 @@ const Collection = () => {
         <Center w="100vw">
           <Grid className="collection_grid" >
           <GridItem >
-            <Image w={[200,300, 400]} src='https://images.dailyobjects.com/marche/assets/images/other/dremscape-banner-mob-02.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
+            <Image w={[200,300, 350]} src='https://images.dailyobjects.com/marche/assets/images/other/dremscape-banner-mob-02.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
             <Text className="card-text" fontSize={{ base: '8px', md: '10px', lg: '16px' }}>DREAMSCAPE</Text>
             <Text className="card-text" fontSize={{ base: '5px', md: '10px', lg: '15px' }}>A vivid dream journal that lets you carry your daydreaming stance into real life.</Text>
             <Link color={"black"} fontSize={{ base: '8px', md: '10px', lg: '16px' }}>Shop Now</Link>
           </GridItem>
 
           <GridItem >
-            <Image w={[200,300, 400]} src='https://images.dailyobjects.com/marche/assets/images/other/zodiac-collections.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
+            <Image w={[200,300, 350]} src='https://images.dailyobjects.com/marche/assets/images/other/zodiac-collections.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
             <Text className="card-text" fontSize={{ base: '8px', md: '10px', lg: '16px' }}>ZODIAC</Text>
             <Text className="card-text" fontSize={{ base: '5px', md: '10px', lg: '15px' }}>Zodiac Collection reflects the unique personality of every individual</Text>
             <Link color={"black"}fontSize={{ base: '8px', md: '10px', lg: '16px' }}>Shop Now</Link>
           </GridItem>
 
           <GridItem >
-            <Image w={[200,300, 400]} src='https://images.dailyobjects.com/marche/assets/images/other/pixel-banner-4-02.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
+            <Image w={[200,300, 350]} src='https://images.dailyobjects.com/marche/assets/images/other/pixel-banner-4-02.jpg?tr=cm-pad_crop,v-2,w-421,dpr-1' />
             <Text className="card-text" fontSize={{ base: '8px', md: '10px', lg: '16px' }}>PIXEL</Text>
             <Text className="card-text" fontSize={{ base: '5px', md: '10px', lg: '15px' }}>A reflection of modern pop-culture, this collection has a vintage appeal that evokes nostalgia.</Text>
             <Link color={"black"}fontSize={{ base: '8px', md: '10px', lg: '16px' }}>Shop Now</Link>
