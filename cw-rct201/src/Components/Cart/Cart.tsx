2a691ed49459f94b14657483d810b08eb4780015
palabhi017 <palabhi017@gmail.com>
2023-01-22 11:42:44 +0530

    temp

--- cw-rct201/src/Components/Cart/Cart.tsx
+++ cw-rct201/src/Components/Cart/Cart.tsx
@@ -5,9 +5,9 @@ import { BsBagCheckFill } from "react-icons/bs";
 import {BiLockAlt} from "react-icons/bi"
 const Cart = () => {
        const [total,settotal] = useState<number>(0)
-     const handletotal =(temp:number)=>{
-        settotal((pre)=> pre+=temp)
-     }
+    //  const handletotal =(temp:number)=>{
+    //     settotal((pre)=> pre+=temp)
+    //  }
 
   return (
 
@@ -20,8 +20,8 @@ const Cart = () => {
         
         <HStack justifyContent={"space-between"} p="10px 30px" mt="20px" w="100%" h="auto">
         <Grid w="60%" h="auto" p="10px 40px" templateColumns={"repeat(2,40%)"} gap="40px" border="1px solid #999" >
-            <CartItems  handletotal={handletotal}  />
-            <CartItems  handletotal={handletotal}  />
+            <CartItems    />
+            <CartItems   />
         </Grid>
         <VStack pos="absolute" borderRadius={"5px"} boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" border="1px solid #161636" gap="20px" h="200px"  top="180px" left="65%" w="30%">
             <Text color="#161636" fontWeight={"bold"} fontSize="20px" mt="10%">Total price :- {"    "} {total}</Text>
