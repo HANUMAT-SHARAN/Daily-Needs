0e895ff7ffe69f50c9e6a197f80ee0573587cd0f
sonu25122000 <skg74827@gmail.com>
2023-03-02 16:58:26 +0530

    checkout

--- cw-rct201/src/App.tsx
+++ cw-rct201/src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import Navbar from './Components/Navbar/Navbar';
 import SingleProduct from './Components/SingleProduct';
+import Checkout from './Pages/chekout';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/>
      <AllRoutes/> */}
-     <SingleProduct/>
+     {/* <SingleProduct/> */}
+     <Checkout/>
     </div>
   );
 }
