dac500e8f4fb3658599975c8b612c8f56fce10e3
sonu25122000 <skg74827@gmail.com>
2023-03-04 18:37:41 +0530

    sidebar reponsivness added

--- cw-rct201/src/sonu/mainPage1.tsx
+++ cw-rct201/src/sonu/mainPage1.tsx
@@ -1,21 +1,12 @@
 import {
-  Box,
-  Checkbox,
-  CheckboxGroup,
-  Divider,
-  Flex,
-  Heading,
-  Image,
-  Spacer,
-  Stack,
+  Box, Flex
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
 import { ProductGrid } from "./ProductGrid";
 import axios from "axios";
-// import "../Components/MobileProducts/Card.css";
-// import "../Components/MobileProducts/Card.css"
 import { Link } from "react-router-dom";
+import SimpleSidebar from "./sidebar";
 export interface Data {
   image1: string;
   image2: string;
@@ -32,14 +23,6 @@ export interface Data {
   cat: string;
 }
 const MainPage1 = () => {
-  // const getData = async (category, rating) => {
-  //   return await axios
-  //     .get(
-  //       `https://koti-api.onrender.com/iphone`
-  //     )
-  //     .then((res) => setProducts(res.data));
-  // };
-  // console.log(products)
 
   const [Iphone, setIphone] = useState<Data[]>([]);
   const [category, setcategory] = useState<string>("Apple");
@@ -58,54 +41,11 @@ const MainPage1 = () => {
   return (
     <div>
       <Flex>
+        <Box>
+        <SimpleSidebar children={undefined}/>
+        </Box>
         <Box
-          width={"20%"}
-          borderWidth="1px"
-          rounded="lg"
-          shadow="lg"
-          paddingLeft={"3%"}
-          paddingTop={"3%"}
-          bg={"#ECF2FF"}
-          h={"85vh"}
-          // zIndex="2"
-          position="relative"
-        >
-          {/* <button onClick={()=>setcategory("Apple")}>Apple</button>
-          <button onClick={()=>setcategory("samsung")}>Samsung</button>
-          <button onClick={()=>setcategory("OnePlus")}>Oneplus</button>
-          <button onClick={()=>setcategory("Oppo")}>Oppo</button>
-          <button onClick={()=>setcategory("Motorola")}>Motorola</button>
-          <button onClick={()=>setcategory("Google")}>Google</button>
-          <button onClick={()=>setrating("desc")} >Asc</button> */}
-          
-          <CheckboxGroup colorScheme="green">
-            <Stack spacing={"20px"}>
-
-            <Heading size={"md"} color={"green"}>Category</Heading>
-            <Stack direction={"column"}>
-              <Checkbox  onChange={()=>setcategory("Apple")} >Apple</Checkbox>
-              <Checkbox onChange={()=>setcategory("samsung")}>Samsung</Checkbox>
-              <Checkbox onChange={()=>setcategory("OnePlus")} >Oneplus</Checkbox>
-              <Checkbox onChange={()=>setcategory("Oppo")} >Oppo</Checkbox>
-              <Checkbox onChange={()=>setcategory("Motorola")} >Motorola</Checkbox>
-              <Checkbox onChange={()=>setcategory("Google")} >Google</Checkbox>
-            </Stack>
-            </Stack>
-          </CheckboxGroup>
-          <Divider border={"2px solid "} width={"100px"} orientation='horizontal' />
-          <CheckboxGroup colorScheme="green">
-            <Stack spacing={"20px"}>
-            <Heading size={"md"} color={"green"}>Fiter By Price</Heading>
-            <Stack direction={"column"}>
-              <Checkbox  onChange={()=>setrating("desc")} >High-Low</Checkbox>
-              <Checkbox onChange={()=>setrating("asc")}>Low-High</Checkbox>
-            </Stack>
-            </Stack>
-          </CheckboxGroup>
-          </Box>
-        <Spacer />
-        <Box
-          w="78%"
+        width={"80%"}
           borderWidth="1px"
           rounded="lg"
           shadow="lg"
